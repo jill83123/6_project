9f6519e2eb723d846d7d0ae6be0dd368bcfd5540
FANG <71370244+jill83123@users.noreply.github.com>
2023-04-28 14:36:06 +0800

    0428

---
+++ 02_JQUERY/js/all.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+    
+  $('.list > li > a').click(function(event) {
+    event.preventDefault();
+
+    $(this).toggleClass('active');
+    $(this).parent().siblings().find('a').removeClass('active');
+    
+    $(this).parent().find('ul').slideToggle();
+    $(this).parent().siblings().find('ul').slideUp();
+  });
+
+  $('.Top-arrow-Icon').click(function(event) {
+    event.preventDefault();
+    $('html,body').animate({
+      scrollTop: 0
+    }, 1000);
+   });
+
+  //swiper
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    effect: 'fade',
+
+    autoplay: {
+      delay: 2000,
+    },
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+
+    // Navigation arrows
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+  });
+
+  //lightbox
+  lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': false,
+    'disableScrolling':true,
+  })
+
+});
