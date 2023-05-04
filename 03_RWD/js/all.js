d9a851cb2415e55769d42a9a71ffe53b447e68cd
FANG <71370244+jill83123@users.noreply.github.com>
2023-05-05 00:59:39 +0800

    0504

--- 03_RWD/js/all.js
+++ 03_RWD/js/all.js
@@ -1,6 +1,19 @@
-$(document).ready(function() {
-    $('.ham-menu').on('click',  function(e){
-       e.preventDefault();
-       $('.nav').toggleClass('menu-show');
-   });
- });
+$(document).ready(function () {
+  $('.ham-menu').on('click', function (e) {
+    e.preventDefault();
+    $('.nav').toggleClass('menu-show');
+  });
+
+  $('.sort').on('click', function (e) {
+    e.preventDefault();
+    $('.content-l').toggleClass('sort-show');
+  });
+
+  $('.heart').on('click', function (e) {
+    e.preventDefault();
+    $('.heart i').toggleClass('fa-solid red');
+    $(this).parent().siblings().find('.heart i').removeClass('fa-solid red');
+  });
+
+
+});
